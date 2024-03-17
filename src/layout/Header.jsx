18a9c3e2d5a551992cf2cb09e8af6296720ba0d8
Алексей <zaykuh@gmail.com>
2024-03-17 21:41:13 +0500

    feat: add Header

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+    return <nav>
+            <div className="nav-wrapper deep-purple lighten-2">
+                <a href="http://localhost:3000/" className="brand-logo">Fortnite shop</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+    </nav>
+    
+}
+
+export {Header} 
